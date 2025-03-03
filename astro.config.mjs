124db11c4ea69ff797a4b52b871107d8a2667436
endigo9740 <gundamx9740@gmail.com>
2025-03-03 12:31:42 -0600

    Swap from Tailwind Vite to PostCSS plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [],
   },
 });
