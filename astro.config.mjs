38aa4c03b4aa343b855904f2f57a9bb7c4124887
endigo9740 <gundamx9740@gmail.com>
2025-03-03 12:56:16 -0600

    Back to the Tailwind Vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    plugins: [],
+    plugins: [tailwindcss()],
   },
 });
